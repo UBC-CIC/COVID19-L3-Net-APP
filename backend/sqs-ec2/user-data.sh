a997a6a678b24c5a586e11c973fc0bb301910a19
Rodrigues <artrodri@a483e77799ff.ant.amazon.com>
2021-01-22 10:20:16 -0800

    adding cloudwatch agent

--- backend/sqs-ec2/user-data.sh
+++ backend/sqs-ec2/user-data.sh
@@ -3,22 +3,15 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 ACCOUNT="$(aws sts get-caller-identity --query Account --output text)"
 REGION="$(curl -s http://169.254.169.254/latest/meta-data/local-hostname | cut -d . -f 2)"
 
-# ${sqsUrl} ${logGroup} ${cloudfrontDomain} ${gitBranch}
+# ${sqsUrl} ${cloudfrontDomain} ${gitBranch}
 
 SQSQUEUE=$1
-CLOUDWATCHLOGSGROUP=$2
-CLOUDFRONT=$3
-GITBRANCH=$4
+CLOUDFRONT=$2
+GITBRANCH=$3
 
 WORKING_DIR=/root/covid-19-app-${GITBRANCH}/backend/sqs-ec2
 
-logger "$0: -------------- Initializing user-data.sh Account: ${ACCOUNT} - Region: ${REGION} - Queue: ${SQSQUEUE} - Logs: ${CLOUDWATCHLOGSGROUP} - CDN: ${CLOUDFRONT} - Branch: ${GITBRANCH}"
-
-yum -y --security update
-yum -y update aws-cli
-
-# This fixes awslogsd.service error (ImportError: cannot import name _normalize_host)
-pip install --user sphinx
+logger "$0: -------------- Initializing user-data.sh Account: ${ACCOUNT} - Region: ${REGION} - Queue: ${SQSQUEUE} - CDN: ${CLOUDFRONT} - Branch: ${GITBRANCH}"
 
 aws configure set default.region $REGION
 
@@ -28,12 +21,6 @@ cp -av $WORKING_DIR/spot-instance-interruption-notice-handler.sh /usr/local/bin/
 cp -av $WORKING_DIR/worker.sh /usr/local/bin
 chmod +x /usr/local/bin/spot-instance-interruption-notice-handler.sh
 chmod +x /usr/local/bin/worker.sh 
-
-sed -i "s|us-east-1|$REGION|g" /etc/awslogs/awscli.conf
-sed -i "s|%CLOUDWATCHLOGSGROUP%|$CLOUDWATCHLOGSGROUP|g" /etc/awslogs/awslogs.conf
-
-systemctl start amazon-ssm-agent
-systemctl start awslogsd
 
 sed -i "s|%REGION%|$REGION|g" /usr/local/bin/worker.sh
 sed -i "s|%SQSQUEUE%|$SQSQUEUE|g" /usr/local/bin/worker.sh
@@ -49,5 +36,5 @@ fi
 docker run --runtime nvidia -p 80:80 --network 'host' -d --restart always covid-19-api:${IMAGE_TAG}
 
 #systemctl start spot-instance-interruption-notice-handler
-logger "$0: -------------- Starting worker"
+logger "$0: -------------- Starting worker --------------"
 systemctl start worker
